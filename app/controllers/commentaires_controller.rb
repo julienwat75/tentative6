b5c1fed1fe13c7014ee99563b6f89c148d2ef6df
julienwat75 <julien_wat@hotmail.fr>
2013-10-20 16:38:45 +0200

    manuel3

--- app/controllers/commentaires_controller.rb
+++ app/controllers/commentaires_controller.rb
@@ -1,10 +1,12 @@
 class CommentairesController < ApplicationController
 def create              # le submit va chercher la methode create
+
+ #@user=current_user
   @commentaires = Commentaire.new(commentaire_params)
  
    @commentaires.author_id = params[:author_id]
 
-
+   @commentaires.author_name=current_user.username
   
   @commentaires.save   #on sauvegarde
    redirect_to author_path(@commentaires.author_id)     # redirection vers le show du profil
@@ -12,7 +14,7 @@ def create              # le submit va chercher la methode create
 end
 
 def commentaire_params
-    params.require(:commentaire).permit(:author_name, :body)
+    params.require(:commentaire).permit(:body)
   end
 
 
