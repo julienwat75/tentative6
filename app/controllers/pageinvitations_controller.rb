4845841e5440196d26ad851256943884c4428ef6
julien <julien_wat@hotmail.fr>
2013-12-21 17:42:18 +0100

    ajout champs code4

--- app/controllers/pageinvitations_controller.rb
+++ app/controllers/pageinvitations_controller.rb
@@ -3,14 +3,14 @@ class PageinvitationsController < ApplicationController
 def index
 
 a=0
-
+@page_suivante=a.to_i+1
 
 @invitations2=Invitation.find(:all,
                      :conditions => "",
                       :order      =>  "dateinvitation",
-                      :limit      =>  4,
+                      :limit      =>  2,
                        :offset      =>  a*2)     # numero de page qu on souhaite afficher
- 4
+ 
 
 end
 
@@ -21,13 +21,13 @@ end
 def show
 
     a=params[:id]
-
+    @page_suivante=a.to_i+1
 
 @invitations2=Invitation.find(:all,
                      :conditions => "",
                       :order      =>  "dateinvitation",
-                      :limit      =>  4,
-                       :offset      =>  a.to_i*4)     # a.to_i converti la string en int
+                      :limit      =>  2,
+                       :offset      =>  a.to_i*2)     # a.to_i converti la string en int
  
 
   end
