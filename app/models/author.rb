58c89675ea934315ffbd33ffeb337a658085cd08
julien <julien_wat@hotmail.fr>
2013-12-23 03:11:01 +0100

    hello15

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
 
-  attr_accessible :username, :email, :avatar, :nom, :prenom, :crypted_password, :sexe
+  attr_accessible :username, :email, :avatar, :nom, :prenom, :password, :password_confirmation, :sexe
   authenticates_with_sorcery!
    has_many :commentaires
    has_many :invitations
