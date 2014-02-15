98bb0a199935417c1e1edad653e6f67d4d18759b
julien <julien_wat@hotmail.fr>
2014-02-15 01:22:42 +0100

    mdp perdue2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,8 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
+config.action_mailer.default_url_options = { :host => 'http://billetgratuit.herokuapp.com' }
+
 
 config.paperclip_defaults = {
   :storage => :s3,
