68ffd86bb101341d135a559988b84f51d3dfea41
julien <julien_wat@hotmail.fr>
2014-03-09 16:02:31 +0100

    test

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -17,6 +17,84 @@ end
 
 def create
   # Amount in cents
+
+  #------reservation site --------
+
+if current_user.limite 
+
+  #redirect_to "/pages/limite"
+  #return false
+
+  end
+
+
+
+
+
+
+  
+  code1=Invitation.aleatoire
+  
+  
+ @reservations = Reservation.new
+ id=params[:idinvitation]
+ @invitation=Invitation.find(params[:idinvitation])
+
+  @tabmulti=@invitation.multidates
+  @multi=@tabmulti.find_by_id(params[:idmulti])
+
+    
+  
+  
+  places_demander=params[:nombreinvitations]
+  places_restante=(@multi.placex.to_i)-(places_demander.to_i)   # on soustrait le nombre de place
+
+
+  
+
+  if places_restante < 0 
+
+  redirect_to "/pages/epuise"
+
+  else
+
+  
+  current_user.update_attribute(:limite, true)
+
+
+  @multi.update_attribute(:placex, places_restante)
+ 
+  
+
+ 
+  @reservations.nombreinvitations=params[:nombreinvitations] 
+  @reservations.titre=params[:titre]   # ici on recupere directement le champ du form ds l'id car ce n est pas un f.text_field 
+  @reservations.dateinvitation=params[:dateinvitation] 
+  @reservations.adresse=params[:adresse] 
+  @reservations.vraiadresse=params[:vraiadresse]  
+  @reservations.emailpartenaire=params[:emailpartenaire]  
+  @reservations.author_id =current_user.id
+
+  @reservations.invitation_id =params[:idinvitation]
+  @reservations.email_membre =current_user.username
+  @reservations.pseudo =current_user.email
+  @reservations.author_nom =current_user.nom
+  @reservations.author_prenom =current_user.prenom
+  @reservations.sexe =current_user.sexe
+  @reservations.code=code1
+  @reservations.envoiemail = "true"
+  
+  @reservations.save   #on sauvegarde
+
+   Notifier.send_resa_email(@reservations).deliver
+
+    
+   #redirect_to "/pages/confirmation"
+
+ end
+
+
+  #--------------------------------
   
   @parametre=params
   @info=params[:stripeToken]
@@ -47,5 +125,12 @@ rescue Stripe::CardError => e
 
 
 end
+
+
+
+def reservation_params
+    params.require(:reservation).permit(:nombreinvitations)
+  end
+
 
 end
